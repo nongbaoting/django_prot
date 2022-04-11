6bea18525bca4afd3e4a1984266b2294febc19f9
nongbaoting <523135753@qq.com>
2022-04-11 13:59:55 +0800

    routine backup 20220411

--- gitback.sh
+++ gitback.sh
@@ -1,2 +1,9 @@
+#/bin/bash
 cd /dat1/nbt2/server/PROTsim/django_prot
 git add -A
+git commit -m 'routine backup '`date +%Y%m%d`
+git push -u origin main
+/dat1/nbt2/server/PROTsim/prot
+git add -A
+git commit -m 'routine backup '`date +%Y%m%d`
+git push -u origin main
