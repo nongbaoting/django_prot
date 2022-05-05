6a8ff851f35c39b271cc08e93e776dd1391e2854
nongbaoting <523135753@qq.com>
2022-05-06 03:00:01 +0800

    routine backup 20220506

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 # conda activate webProt
+# /dat1/nbt2/server/PROTsim/public/django_prot
 python run_predict.py watch_blast > logs/blast.log 2>&1 &
 celery -A django_prot worker -l info  -c 4 -E > logs/celery.log 2>&1 &
 python manage.py runserver 222.200.186.124:3389
