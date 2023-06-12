6ac8a82252834420c105bc25221122d4cbf7c925
nongbaoting <523135753@qq.com>
2023-06-13 03:00:01 +0800

    routine backup 20230613

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+# 
+/dat1/nbt2/server/PROTsim/django_prot/django_prot/celeryconfig.py
+broker_url ='redis://localhost:6379/1'
+
 # conda activate webProt
 ### 更新
 # cd /dat1/nbt2/server/PROTsim/public/
@@ -5,7 +9,7 @@
 # cp -r ../django_prot .
 cd /dat1/nbt2/server/PROTsim/public/django_prot
 conda activate webProt
-python run_predict.py watch_blast > logs/blast.log 2>&1 &
+#python run_predict.py watch_blast > logs/blast.log 2>&1 &
 celery -A django_prot worker -l info  -c 4 -E > logs/celery.log 2>&1 &
 python manage.py runserver 172.22.148.191:3389
 
@@ -15,9 +19,10 @@ npm run build
 rm -rf /training/nong/protein/db/web/protein
 cp -r /dat1/nbt2/server/PROTsim/prot/dist/ /training/nong/protein/db/web/protein
 
-# 47 启动 alphafold
+# 150(47) 启动 alphafold
+## cp -r /dat1/nbt2/server/PROTsim/public/django_prot /training/nong/web/django_prot
 cd /training/nong/web/django_prot
-conda activate webProt
+conda activate webProt2
 python ./run_predict.py watchdog > logs/logs.txt 2>&1 &
 
 # dev
